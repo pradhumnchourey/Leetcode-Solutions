008558faafefeb7a7ded34028c1b6fc605b82a42
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-04-26 18:08:47 +0530

    Time: 3 ms (56.14%), Space: 6 MB (63.10%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if(num==0)
-            return 0;
-        if(num%9==0)
-            return 9;
-        return num%9;
+        int sum=0;
+        while(num){
+            sum+=(num%10);
+            num/=10;
+        }
+        if(sum<10)
+            return sum;
+        else return addDigits(sum);
     }
 };
