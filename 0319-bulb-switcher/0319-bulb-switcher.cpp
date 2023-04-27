cc69fc391ba0e6eed1f480e962cc0801a4845713
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-04-27 20:45:38 +0530

    Time: 0 ms (100.00%), Space: 6 MB (63.99%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        if(n<2)
-            return n;
-        int ans=1;
-        while(ans<n){
-            if(ans*(ans+2)<n)
-                ans++;
-            else return ans;
-        }
-        return n;
+        return (int)sqrt(n);
     }
 };
