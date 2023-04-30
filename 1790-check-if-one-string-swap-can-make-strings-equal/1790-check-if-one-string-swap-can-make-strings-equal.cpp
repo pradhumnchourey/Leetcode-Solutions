d2e708ca3c85227f50eb52686025a6ce8014c711
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-04-30 21:05:32 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (86.47%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        if(s1==s2)
-            return true;
-        string s3=s1, s4=s2;
-        sort(s3.begin(), s3.end());
-        sort(s4.begin(), s4.end());
-        if(s3!=s4)
-            return false;
-        int len = s1.length();
-        map<int, char> mpp1, mpp2;
-        for(int i=0; i<len; i++){
-            mpp1[i]=s1[i];
-            mpp2[i]=s2[i];
+        int cnt = 0 ; 
+        for (int i = 0 ; i < s1.length() ; i ++){
+            if (s1[i] != s2[i]){
+                cnt ++ ;
+            }
+            else continue;
         }
-        int count=0;
-        for(int i=0; i<mpp1.size(); i++){
-            if(mpp1[i]!=mpp2[i])
-                count++;
+        sort(s1.begin(), s1.end()); 
+        sort(s2.begin(), s2.end()); 
+        if (s1 != s2){
+            return false ; 
         }
-        return count==2 ? true : false;
+        return (cnt==2 || cnt==0) ? true:false;
     }
 };
