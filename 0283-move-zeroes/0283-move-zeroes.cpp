318245b74f0af9909b799ce9da4897fd725137e6
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-05-03 18:08:04 +0530

    Time: 95 ms (11.82%), Space: 19.4 MB (17.21%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n=nums.size();
-        int j = 0;
-        for (int i = 0; i < n; i++){
-            if (nums[i] != 0){
-                nums[j] = nums[i];
-                j++;
+        int count=0;
+        for(int i=0; i<nums.size();){
+            if(nums[i]==0){
+                count++;
+                nums.erase(nums.begin()+i);
             }
+            else i++;
         }
-        for (int i = j; j < n; j++){
-            nums[j] = 0;
-        }
+        while(count--)
+            nums.push_back(0);
     }
 };
