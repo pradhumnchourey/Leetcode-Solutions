e990247dfc8c6e530154eeecdd4b689d2791aa47
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-05-04 11:59:29 +0530

    Time: 6 ms (83.21%), Space: 8.1 MB (12.21%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     string predictPartyVictory(string senate) {
-        vector<char> s;
-        for(char ch: senate){
-            s.push_back(ch);
+        queue<int> qr, qd;
+        int n = senate.length();
+
+        for(int i = 0; i<n; i++)
+            (senate[i] == 'R')?qr.push(i):qd.push(i);
+
+        while(!qr.empty() && !qd.empty()){
+            int r_id = qr.front(), d_id = qd.front();
+            qr.pop(), qd.pop();
+            (r_id < d_id)?qr.push(r_id + n):qd.push(d_id + n);
         }
-        while(!s.empty()){
-            for(int i=0; i<s.size(); i++){
-                if(s[i]=='R'){              //if the ith element is R
-                    auto temp = find(s.begin(), s.end(), 'D');  //find D in the vector
-                    if(temp==s.end())       //if D is not present
-                        return "Radiant";   //Radiant has won the game
-                    //else search for D, from that index, in rest of the array
-                    auto temp1 = find(s.begin()+i, s.end(), 'D');   
-                    if(temp1!=s.end())      //if D is present in rest of the array
-                        s.erase(temp1);     //delete it
-                    else if(temp!=s.end())  //else if D is present in the array anywhere
-                        s.erase(temp);      //delete its first occurance
-                }
-                else{                       //if the ith element is D
-                    auto temp = find(s.begin(), s.end(), 'R');      //find R in the vector
-                    if(temp==s.end())       //if R is not present
-                        return "Dire";      //Dire has won the game
-                    //else find R in the rest of the array
-                    auto temp1 = find(s.begin()+i, s.end(), 'R');
-                    if(temp1!=s.end())      //if R is present in rest of the array
-                        s.erase(temp1);     //delete it
-                    else if(temp!=s.end())  //else if R is present in the array anywhere
-                        s.erase(temp);      //delete its first occurance
-                }
-            }
-        }
-        return "";
+        
+        return (qr.size() > qd.size())? "Radiant" : "Dire";
     }
 };
