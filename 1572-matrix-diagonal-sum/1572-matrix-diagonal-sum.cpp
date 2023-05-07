aa0597ff9e324d0bfa392b6e2303abbcf0236f5d
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-05-07 18:13:41 +0530

    Time: 22 ms (5.54%), Space: 11.4 MB (17.60%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -3,13 +3,8 @@ public:
     int diagonalSum(vector<vector<int>>& mat) {
         int sum=0;
         int n=mat.size();
-        for(int i=0; i<n; i++){
-            sum+=mat[i][i];
-        }
-        int i=0;
-        for(int j=n-1; j>=0; j--){
-            sum+=mat[i][j];
-            i++;
+        for(int i=0, j=n-1; i<n; i++, j--){
+            sum+=mat[i][i]+mat[i][j];
         }
         if(n%2!=0)
             sum -= mat[n/2][n/2];
