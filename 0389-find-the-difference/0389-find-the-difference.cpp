f9544de2e1f0a98b945d935212ba45e9b6873dd4
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-05-10 03:00:16 +0530

    Time: 3 ms (63.26%), Space: 6.7 MB (72.32%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
-    char findTheDifference(string s, string t) {
-        unordered_map<char, int> mpp;
-        for(auto& ch: s)
-            mpp[ch]++;
-        for(auto& ch: t)
-            if(--mpp[ch]<0)
-                return ch;
+     char findTheDifference(string s, string t) {
+        vector<int> v(26, 0);
+        for(auto& c:s) v[c-'a']+=1;
+        for(auto& c:t) if(--v[c-'a']<0) return c;
         return '\0';
     }
 };
