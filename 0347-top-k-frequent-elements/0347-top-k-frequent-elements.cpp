ca2ba4f97a5b21f8391a777b45072c2b5599a9eb
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-05-22 15:15:11 +0530

    Time: 24 ms (20.39%), Space: 13.7 MB (68.96%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        //first make a map to store the frequencies of each element
-        unordered_map<int,int> m;
-        for(int i = 0; i < nums.size();i++){
-            m[nums[i]]++;
+        
+        int n=nums.size();
+        unordered_map<int,int>mp;
+        vector<int> Res;
+       
+        
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
         }
-    //then store those frequencies, in a set with their values, as pairs<freq, values>
-        set<pair<int,int>> s;
-        for(int i = 0; i < nums.size();i++){
-            s.insert(make_pair(m[nums[i]],nums[i]));
+        
+        priority_queue<pair<int,int>> m;
+        for( auto i : mp){
+            m.push(make_pair( i.second, i.first));
         }
-        vector<int> ans;
-        auto it = --s.end();        //iterator it points to the last element of the set
-        for(int i=0; i<k; i++){     //run the loop k time
-            //push the second value of the pair, i.e. original value from nums
-            ans.push_back((*it).second);
-            it--;
+        
+        for( int i =0;i<k ; i++){
+            Res.push_back( m.top().second);
+            m.pop();
         }
-        return ans;
+      
+        return Res;
     }
 };
