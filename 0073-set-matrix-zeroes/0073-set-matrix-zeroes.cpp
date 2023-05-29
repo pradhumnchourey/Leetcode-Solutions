490ca7484deb974a5841ba513578f2049a7cdb21
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-05-29 23:14:24 +0530

    Time: 12 ms (86.33%), Space: 13.2 MB (87.79%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        //stores the number of rows in the matrix
+        int m=matrix.size();
+        //stores the number of columns in the matrix
+        int n = matrix[0].size();
+        
+        bool row=false, col=false;
+        for(int i=0; i<n; i++)
+            if(matrix[0][i]==0)
+                row=true;
+        for(int i=0; i<m; i++)
+            if(matrix[i][0]==0)
+                col=true;
+        
+        //for each element of the matrix, if it is 0
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]==0){
+                    //set the first element of that column = 0, as a marker
+                    matrix[i][0]=0;
+                    //set the first element of that row = 0, as a marker
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        //now starting from 1st row not the 0th row, check if its first element is 0
+        //set all the elements of that row = 0
+        for(int i=1; i<m; i++)
+            if(matrix[i][0]==0)
+                for(int j=0; j<n; j++)
+                    matrix[i][j]=0;
+        
+        //now starting from 1st column not the 0th column, check if its first element is 0
+        //set all the elements of that column = 0
+        for(int i=1; i<n; i++)
+            if(matrix[0][i]==0)
+                for(int j=0; j<m; j++)
+                    matrix[j][i]=0;
+        
+        //if the 0,0th element is zero set all the elements of that row and column as 0
+        if(matrix[0][0]==0){
+            if(row==true)
+                for(int i=0; i<n; i++)
+                    matrix[0][i]=0;
+            if(col==true)
+                for(int i=0; i<m; i++)
+                    matrix[i][0]=0;
+        }
+    }
+};
