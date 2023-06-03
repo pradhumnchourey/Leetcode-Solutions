bee333a921f672d316615337832ef1309e754256
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-06-03 15:15:42 +0530

    Time: 309 ms (74.31%), Space: 112.8 MB (79.84%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
-    int dfs(int head, int& sum, vector<int>& informTime, unordered_map<int, vector<int>>& adjList){
-        int max_time = 0;
-                
-        //if the ID called head is present in the map, then only call for loop for its subordinates
-        if(adjList.find(head)!=adjList.end())
-            //for each subordinate in the vector at key=head in the map
-            for(auto subordinate: adjList[head])
-                //again call dfs for its subordinate and so on, i.e. depth first search
-                max_time = max(max_time, dfs(subordinate, sum, informTime, adjList));
-        
-        if(max_time == sum)
-            return max_time;
-        
-        //return max_time + informTime of that manager or head
-        return max_time + informTime[head];
+    int dfs(int headId,vector<int>& informTime,vector<int> adj[]){
+
+        int maxTime = 0;
+
+        for(auto it:adj[headId])
+           maxTime = max(maxTime,dfs(it,informTime,adj));
+
+        return informTime[headId] + maxTime;   
     }
-    
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        //map to store all the suboridnates of a manager int a vector
-        unordered_map<int, vector<int>> adjList;
         
-        int sum = accumulate(informTime.begin(), informTime.end(), 0);
-        
-        //iterate for manager vector
-        for(int i=0; i<manager.size(); i++)
-            if(manager[i]!=-1)                      //the head doesn't have any manager
-        //manager[i] gives the manager of ith employee, so in the map at manager[i] means for ith manager, push i, i.e. push this employee
-        //for example at index 1 we have 2, so at key=2 in the push 1, i.e. employee ID 1 is the subordinate of manager with id 2
-                adjList[manager[i]].push_back(i);
-        
-        //now since we have all the subordinates of every employee, call dfs for the head, with informTime and adjList
-        return dfs(headID, sum, informTime, adjList);
+            vector<int> adj[n];
+
+            for(int i=0;i<manager.size();i++)
+                if(manager[i] != -1)
+                  adj[manager[i]].push_back(i);
+            
+        return dfs(headID,informTime,adj);    
     }
 };
