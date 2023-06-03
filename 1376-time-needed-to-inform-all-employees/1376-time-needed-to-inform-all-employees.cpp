5992541393c3e9e53b32e8289fb8922f3e0d10e9
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-06-03 15:14:19 +0530

    Time: 461 ms (27.58%), Space: 139.1 MB (25.25%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
-    int dfs(int head, vector<int>& informTime, unordered_map<int, vector<int>>& adjList){
+    int dfs(int head, int& sum, vector<int>& informTime, unordered_map<int, vector<int>>& adjList){
         int max_time = 0;
-        
+                
         //if the ID called head is present in the map, then only call for loop for its subordinates
         if(adjList.find(head)!=adjList.end())
             //for each subordinate in the vector at key=head in the map
             for(auto subordinate: adjList[head])
                 //again call dfs for its subordinate and so on, i.e. depth first search
-                max_time = max(max_time, dfs(subordinate, informTime, adjList));
+                max_time = max(max_time, dfs(subordinate, sum, informTime, adjList));
+        
+        if(max_time == sum)
+            return max_time;
         
         //return max_time + informTime of that manager or head
         return max_time + informTime[head];
@@ -18,6 +21,8 @@ public:
         //map to store all the suboridnates of a manager int a vector
         unordered_map<int, vector<int>> adjList;
         
+        int sum = accumulate(informTime.begin(), informTime.end(), 0);
+        
         //iterate for manager vector
         for(int i=0; i<manager.size(); i++)
             if(manager[i]!=-1)                      //the head doesn't have any manager
@@ -26,6 +31,6 @@ public:
                 adjList[manager[i]].push_back(i);
         
         //now since we have all the subordinates of every employee, call dfs for the head, with informTime and adjList
-        return dfs(headID, informTime, adjList);
+        return dfs(headID, sum, informTime, adjList);
     }
 };
