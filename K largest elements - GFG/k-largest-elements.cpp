f9c1634cd2cfcf1866b861b0b6c2417f7c3a754e
Pradhumn Chourey <85006946+pradhumnchourey@users.noreply.github.com>
2023-06-09 18:05:57 +0530

    Added solution - LeetHub

--- K largest elements - GFG/k-largest-elements.cpp
+++ K largest elements - GFG/k-largest-elements.cpp
@@ -11,19 +11,31 @@ class Solution
     //Function to return k largest elements from an array.
     vector<int> kLargest(int arr[], int n, int k)
     {
+        //create a min heap
         priority_queue<int, vector<int>, greater<int>> pq;
+        //push first k elements into the min heap
         for(int i=0; i<k; i++){
             pq.push(arr[i]);
         }
+        //now for elements from k-n, if the ith element of array is greater than
+        //top(smallest) element of the min heap, remove it from the heap
+        //and insert the ith element in the pq
+        //in this manner we have k largest elements of
         for(int i=k; i<n; i++){
             if(arr[i]>pq.top()){
                 pq.pop();
                 pq.push(arr[i]);
             }
         }
-        vector<int> res;
-        for(int i=0; i<k; i++){
-            res.insert(res.begin(), pq.top());
+        
+        //for k largest elements return res vector, for kth largest element return pq.top()
+        
+        //create a result vector of size k and initialize with 0
+        vector<int> res(k,0);
+        
+        //set ith element of vector res as pq.top
+        for(int i=k-1; i>=0; i--){
+            res[i] = pq.top();
             pq.pop();
         }
         return res;
